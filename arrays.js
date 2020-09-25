e7f8470807e783679c3e93e6508a3ed15c79cc8e
positive-proc-6135 <haydenmatz97@gmail.com>
2020-09-25 15:49:20 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
 
 function addElementToBeginningOfArray(A,E){
-var Solution = [...A,E];
+var Solution = [E,...A];
 return Solution;
 }
